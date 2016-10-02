c38d4270094ead5d212763d081ad3b8ade433c7d
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2016-10-02 10:12:27 -0400

    Create Tests project (xunit)

---
+++ test/Podsync.Tests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NuGet.Packaging;
+using Xunit;
+
+namespace Podsync.Tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
