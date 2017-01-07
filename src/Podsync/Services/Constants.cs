ab206fb09d0c83060b5f37c3a2da1f88bf8cb92e
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-01-06 17:47:42 -0800

    Rework Redis serialization

--- src/Podsync/Services/Constants.cs
+++ src/Podsync/Services/Constants.cs
@@ -1,7 +1,11 @@
-﻿namespace Podsync.Services
+﻿using Podsync.Services.Resolver;
+
+namespace Podsync.Services
 {
     public static class Constants
     {
         public const int DefaultPageSize = 50;
+
+        public const ResolveType DefaultFormat = ResolveType.VideoHigh;
     }
 }
