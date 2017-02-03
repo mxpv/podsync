1865b2524b94d5d9ff2f5897772dcd46cecffad8
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-02-03 12:37:56 -0800

    Add temporary workaround for Error -4047 EPIPE broken pipe

--- src/Podsync/Program.cs
+++ src/Podsync/Program.cs
@@ -8,7 +8,15 @@ namespace Podsync
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel(options => options.AddServerHeader = false)
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+
+                    // Temporary workaround for Error -4047 EPIPE broken pipe
+                    // Remove this after upgrade to 1.1.0
+                    // See https://github.com/aspnet/KestrelHttpServer/issues/1182
+                    options.ThreadCount = 1;
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
