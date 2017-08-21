1df178321712022f05a853fb32fa05e33ab13f27
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-08-20 19:26:25 -0700

    Use m4a for audio

--- pkg/builders/common.go
+++ pkg/builders/common.go
@@ -16,8 +16,8 @@ func makeEnclosure(feed *api.Feed, id string, lengthInBytes int64) (string, itun
 	ext := "mp4"
 	contentType := itunes.MP4
 	if feed.Format == api.AudioFormat {
-		ext = "mp3"
-		contentType = itunes.MP3
+		ext = "m4a"
+		contentType = itunes.M4A
 	}
 
 	url := fmt.Sprintf("http://podsync.net/download/%s/%s.%s", feed.HashId, id, ext)
