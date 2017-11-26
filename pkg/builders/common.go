1f11f74302d62d73890d5d473bb815e358e254c0
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-11-26 00:31:48 -0800

    Use http for download links

--- pkg/builders/common.go
+++ pkg/builders/common.go
@@ -21,6 +21,6 @@ func makeEnclosure(feed *model.Feed, id string, lengthInBytes int64) (string, it
 		contentType = itunes.M4A
 	}
 
-	url := fmt.Sprintf("https://podsync.net/download/%s/%s.%s", feed.HashID, id, ext)
+	url := fmt.Sprintf("http://podsync.net/download/%s/%s.%s", feed.HashID, id, ext)
 	return url, contentType, lengthInBytes
 }
