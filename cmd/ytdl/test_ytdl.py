975bc30edf88858307c8fe16027f5cd803629e16
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2018-09-17 19:55:05 -0700

    Update youtube-dl, fix resolve issues

--- cmd/ytdl/test_ytdl.py
+++ cmd/ytdl/test_ytdl.py
@@ -5,13 +5,19 @@ import unittest
 class TestYtdl(unittest.TestCase):
     def test_resolve(self):
         self.assertIsNotNone(
-            ytdl._resolve('https://youtube.com/watch?v=ygIUF678y40', {'format': 'video', 'quality': 'low', 'provider': 'youtube'}))
+            ytdl._resolve('https://youtube.com/watch?v=ygIUF678y40',
+                          {'format': 'video', 'quality': 'low', 'provider': 'youtube'}))
         self.assertIsNotNone(
-            ytdl._resolve('https://youtube.com/watch?v=WyaEiO4hyik', {'format': 'audio', 'quality': 'high', 'provider': 'youtube'}))
+            ytdl._resolve('https://youtube.com/watch?v=WyaEiO4hyik',
+                          {'format': 'audio', 'quality': 'high', 'provider': 'youtube'}))
+        self.assertIsNotNone(
+            ytdl._resolve('https://youtube.com/watch?v=5mjUF2j9dgA',
+                          {'format': 'video', 'quality': 'low', 'provider': 'youtube'})
+        )
 
     def test_vimeo(self):
         self.assertIsNotNone(
             ytdl._resolve('https://vimeo.com/237715420', {'format': 'video', 'quality': 'low', 'provider': 'vimeo'}))
         self.assertIsNotNone(
-            ytdl._resolve('https://vimeo.com/275211960', {'format': 'video', 'quality': 'high', 'provider': 'vimeo' })
+            ytdl._resolve('https://vimeo.com/275211960', {'format': 'video', 'quality': 'high', 'provider': 'vimeo'})
         )
