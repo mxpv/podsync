19bea2f863dc5fd521fd9bde7ab1ad9a44b98dad
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2019-06-02 13:55:44 -0700

    Fix title

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private titleService: Title) {
+    titleService.setTitle('Podsync - Turn YouTube channels into podcast feeds');
+  }
 }
