d67bc99bd2bb9dee5af9d9041c752fc051be207e
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2019-06-02 13:51:22 -0700

    Implement Index page

--- ui/src/app/index/index.component.ts
+++ ui/src/app/index/index.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  loggedIn: boolean;
+  fullName: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.loggedIn = true;
+    this.fullName = 'Full name here';
   }
-
 }
