ae70d989244e3a642ea65f4b696490013728105d
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2019-06-02 17:25:55 -0700

    Add meta tags

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Title} from '@angular/platform-browser';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,37 @@ import {Title} from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, metaService: Meta) {
     titleService.setTitle('Podsync - Turn YouTube channels into podcast feeds');
+
+    metaService.addTag({
+      httpEquiv: 'content-type',
+      content: 'text/html;charset=UTF-8',
+    });
+
+    metaService.addTag({
+      name: 'description',
+      content: 'Simple and free service that lets you listen to any YouTube or Vimeo channels, playlists or user videos in podcast format',
+    });
+
+    metaService.addTag({
+      name: 'og:title',
+      content: 'Podsync - turn YouTube channels into podcast feeds',
+    });
+
+    metaService.addTag({
+      name: 'og:description',
+      content: 'Simple and free service that lets you listen to any YouTube or Vimeo channels, playlists or user videos in podcast format',
+    });
+
+    metaService.addTag({
+      name: 'og:locale',
+      content: 'en_US',
+    });
+
+    metaService.addTag({
+      name: 'og:image',
+      content: '/assets/img/og_image.png',
+    });
   }
 }
