316af91787d5aafe5326ce0ef619e012acfb1c31
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2020-04-15 13:59:52 -0700

    Add OPML unit test

---
+++ pkg/feed/opml_test.go
@@ -0,0 +1,45 @@
+//go:generate mockgen -source=common.go -destination=common_mock_test.go -package=feed
+
+package feed
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mxpv/podsync/pkg/config"
+	"github.com/mxpv/podsync/pkg/model"
+)
+
+func TestBuildOPML(t *testing.T) {
+	expected := `<?xml version="1.0" encoding="UTF-8"?>
+<opml version="1.0">
+	<head>
+		<title>Podsync feeds</title>
+	</head>
+	<body>
+		<outline text="desc" type="rss" xmlUrl="https://url/1.xml" title="1"></outline>
+	</body>
+</opml>`
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	urlMock := NewMockurlProvider(ctrl)
+	urlMock.EXPECT().URL(gomock.Any(), "", "1.xml").Return("https://url/1.xml", nil)
+
+	dbMock := NewMockfeedProvider(ctrl)
+	dbMock.EXPECT().GetFeed(gomock.Any(), "1").Return(&model.Feed{Title: "1", Description: "desc"}, nil)
+
+	cfg := config.Config{
+		Feeds: map[string]*config.Feed{
+			"any": {ID: "1", OPML: true},
+		},
+	}
+
+	out, err := BuildOPML(context.Background(), &cfg, dbMock, urlMock)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, out)
+}
