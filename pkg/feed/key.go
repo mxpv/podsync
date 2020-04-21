7a65e3d1c5639094a0b0f73a1a9305797f206e60
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2020-04-20 19:15:30 -0700

    Implement key providers

---
+++ pkg/feed/key.go
@@ -0,0 +1,51 @@
+package feed
+
+import (
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+type KeyProvider interface {
+	Get() string
+}
+
+type FixedKeyProvider struct {
+	key string
+}
+
+func NewFixedKey(key string) (KeyProvider, error) {
+	if key == "" {
+		return nil, errors.New("key can't be empty")
+	}
+
+	return &FixedKeyProvider{key: key}, nil
+}
+
+func (p FixedKeyProvider) Get() string {
+	return p.key
+}
+
+type RotatedKeyProvider struct {
+	keys  []string
+	lock  sync.Mutex
+	index int
+}
+
+func NewRotatedKeys(keys []string) (KeyProvider, error) {
+	if len(keys) < 2 {
+		return nil, errors.Errorf("at least 2 keys required (got %d)", len(keys))
+	}
+
+	return &RotatedKeyProvider{keys: keys, index: 0}, nil
+}
+
+func (p *RotatedKeyProvider) Get() string {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	current := p.index % len(p.keys)
+	p.index++
+
+	return p.keys[current]
+}
