615f57ce2c5abe9f534ce14a45c0cf34cf1e639c
Paul <paul70079@gmail.com>
2020-10-01 22:02:34 +0200

    fix formatting

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -55,12 +55,12 @@ type Filters struct {
 }
 
 type Custom struct {
-	CoverArt string    `toml:"cover_art"`
-	Category string    `toml:"category"`
-	Explicit bool      `toml:"explicit"`
-	Language string    `toml:"lang"`
-	Author string 	    `toml:"author"`
-	Title string       `toml:"title"`
+	CoverArt    string `toml:"cover_art"`
+	Category    string `toml:"category"`
+	Explicit    bool   `toml:"explicit"`
+	Language    string `toml:"lang"`
+	Author      string `toml:"author"`
+	Title       string `toml:"title"`
 	Description string `toml:"description"`
 }
 
