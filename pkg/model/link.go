8e534f06a9bd7ca25509da47a8d1237817beb7b4
Benjamin Abadie <benjamin.abadie@enix.fr>
2022-08-06 18:57:18 +0200

    feat: add Twitch support

--- pkg/model/link.go
+++ pkg/model/link.go
@@ -15,11 +15,12 @@ const (
 	ProviderYoutube    = Provider("youtube")
 	ProviderVimeo      = Provider("vimeo")
 	ProviderSoundcloud = Provider("soundcloud")
+	ProviderTwitch     = Provider("twitch")
 )
 
 // Info represents data extracted from URL
 type Info struct {
 	LinkType Type     // Either group, channel or user
-	Provider Provider // Youtube, Vimeo, or SoundCloud
+	Provider Provider // Youtube, Vimeo, SoundCloud or Twitch
 	ItemID   string
 }
